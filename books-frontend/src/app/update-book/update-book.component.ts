115f1368e3b3d086f6cbebefc6ab225d9f5b4c57
Tihomir Valchanov <georgi.buyukliev@accedia.com>
2020-12-30 11:37:45 +0200

    Refactor code

--- books-frontend/src/app/update-book/update-book.component.ts
+++ books-frontend/src/app/update-book/update-book.component.ts
@@ -1,10 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Validators, FormGroup, FormBuilder } from '@angular/forms';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Book } from '../book';
-import { Router, ActivatedRoute, ParamMap } from '@angular/router';
-import { first, switchMap } from 'rxjs/operators';
 import { BookService } from '../book.service';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-update-book',
@@ -13,48 +11,12 @@ import { Observable } from 'rxjs';
 })
 export class UpdateBookComponent implements OnInit {
   book: Book;
-  bookForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder,
-    private router: Router,
+  constructor(private router: Router,
     private route: ActivatedRoute,
     private bookService: BookService
   ) {
   }
-
-  // ngOnInit() {
-  //   let bookId = window.localStorage.getItem("book.id");
-  //   if (!bookId) {
-  //     alert("Invalid action.")
-  //     this.router.navigate(['list-user']);
-  //     return;
-  //   }
-  //   this.bookForm = this.formBuilder.group({
-  //     id: [''],
-  //     username: ['', Validators.required],
-  //     firstName: ['', Validators.required],
-  //     lastName: ['', Validators.required],
-  //     age: ['', Validators.required],
-  //     salary: ['', Validators.required]
-  //   });
-  // this.bookService.getBook(+bookId)
-  //   .subscribe(data => {
-  //     this.editForm.setValue(data.result);
-  //   });
-  // }
-
-  // ngOnInit() {
-  //   this.route.params.subscribe(params => {
-  //     this.book.title = params['title'];
-
-  //     this.book = {
-  //       id: null,
-  //       title: params['title'],
-  //       author: params['author'],
-  //       description: params['description'],
-  //       price: params['price']
-  //     }
-  //   });
 
   ngOnInit() {
     this.book = <Book>{};
@@ -67,33 +29,21 @@ export class UpdateBookComponent implements OnInit {
       }, error => console.log(error));
   }
 
-  onSubmit() {
-    // this.apiService.updateUser(this.editForm.value)
-    //   .pipe(first())
-    //   .subscribe(
-    //     data => {
-    //       if (data.status === 200) {
-    //         alert('User updated successfully.');
-    //         this.router.navigate(['list-user']);
-    //       } else {
-    //         alert(data.message);
-    //       }
-    //     },
-    //     error => {
-    //       alert(error);
-    //     });
+  onSubmit(f: NgForm) {
+    this.book = {
+      id: this.book.id,
+      title: f.value.title,
+      author: f.value.author,
+      description: f.value.description,
+      price: f.value.price
+    }
+
+    this.bookService.save(this.book)
+      .subscribe(data => {
+        console.log('Book updated successfully');
+        alert("Book updated successfully.");
+      });
+
+    this.router.navigate(['/books']);
   }
-
-  // update(id: number): void {
-  //   this.bookService.update(this.book);
-  // }
-
-  // getBook(): void {
-  //   const id = +this.route.snapshot.paramMap.get('id');
-  //   this.bookService.getBook(id)
-  //     .subscribe(book => this.book = book);
-  // }
-
-  //submitForm() {this.book$.getValue().title = ""; }
-
 }
