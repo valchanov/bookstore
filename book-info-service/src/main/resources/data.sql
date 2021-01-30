07151f6ae30a3e33026c93af6de1863339e789ad
Tihomir Valchanov <georgi.buyukliev@accedia.com>
2021-01-30 22:30:12 +0200

    Fix dataset

--- book-info-service/src/main/resources/data.sql
+++ book-info-service/src/main/resources/data.sql
@@ -1,7 +1,7 @@
-INSERT INTO book (title, author, description, price) VALUES
-  ('Pride and Prejudice', 'Jane Austen', 'some description goes here', 50.00),
-  ('Brothers Karamazov', 'Fedor Dostoevskiy', 'some description goes here', 40.00),
-  ('War and Peace', 'Leo Tolstoy', 'some description goes here', 22.22),
-  ('Fredrik Backman', 'Author of A Man Called Ove', 'some description goes here', 15.00),
-  ('Fredrik Backman', 'My Grandmother Asked Me to Tell You She is Sorry', 'some description goes here', 20.21),
-  ('Anna Karenina', 'Leo Tolstoy', 'some description goes here', 30.12);
+INSERT INTO book (title, author, description, price)
+VALUES ('Pride and Prejudice', 'Jane Austen', 'some description goes here', 50.00),
+       ('Brothers Karamazov', 'Fedor Dostoevskiy', 'some description goes here', 40.00),
+       ('War and Peace', 'Leo Tolstoy', 'some description goes here', 22.22),
+       ('Author of A Man Called Ove', 'Fredrik Backman', 'some description goes here', 15.00),
+       ('My Grandmother Asked Me to Tell You She is Sorry', 'Fredrik Backman', 'some description goes here', 20.21),
+       ('Anna Karenina', 'Leo Tolstoy', 'some description goes here', 30.12);
